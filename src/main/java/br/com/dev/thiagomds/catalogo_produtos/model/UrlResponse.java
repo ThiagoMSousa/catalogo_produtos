b2f6255259ddb4763e68ed318c47f156b11cc746
Thiago Mendes de Sousa <thiagomendes_95@hotmail.com>
2022-09-17 20:48:51 -0300

    Criando o Model para solicitar a URL Pré-Assinada

---
+++ src/main/java/br/com/dev/thiagomds/catalogo_produtos/model/UrlResponse.java
@@ -0,0 +1,23 @@
+package br.com.dev.thiagomds.catalogo_produtos.model;
+
+public class UrlResponse {
+
+    private String url;
+    private String expirationTime;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getExpirationTime() {
+        return expirationTime;
+    }
+
+    public void setExpirationTime(String expirationTime) {
+        this.expirationTime = expirationTime;
+    }
+}
